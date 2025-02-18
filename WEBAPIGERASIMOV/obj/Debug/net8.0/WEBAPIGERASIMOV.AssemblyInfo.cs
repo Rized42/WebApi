f027fce48f2f072efc4aa3b235fd2b78d636e96b
Герасимов Ростислав <rostislav.gerasimov@gorparkovki.ru>
2025-02-18 15:23:19 +0300

    lf

--- WEBAPIGERASIMOV/obj/Debug/net8.0/WEBAPIGERASIMOV.AssemblyInfo.cs
+++ WEBAPIGERASIMOV/obj/Debug/net8.0/WEBAPIGERASIMOV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBAPIGERASIMOV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8769b81523bde2dddc76166a574c7ae4a5b2f8fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBAPIGERASIMOV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBAPIGERASIMOV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
